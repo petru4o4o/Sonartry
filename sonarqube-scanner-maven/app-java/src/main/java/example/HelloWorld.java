6a57b19ee68b78d88ae785c4a7cb7b7c6afbd489
petru4o4o <petrol89@gmail.com>
2017-06-29 16:02:09 +0300

    ADD unacceptable code

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -16,5 +16,28 @@ public class HelloWorld {
   public void notCovered() {
     System.out.println("notCovered");
   }
+private String name;
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	public void printHello() 
+	{
+		System.out.println("Hello ! " + name);
+	}
+	class MyThread extends Thread
+	{
 
+		@Override
+		public void run()
+		{
+			synchronized(this)
+			{
+			// ...
+			notify();  // Noncompliant
+			}
+		}
+	}
 }
