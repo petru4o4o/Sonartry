2cb5c026f7baac34278573fc2ec928ede8408eb2
petru4o4o <petrol89@gmail.com>
2017-06-30 16:41:13 +0300

    add hello worls 2 with not overrided methods

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -18,3 +18,18 @@ public class HelloWorld {
   }
 
 }
+
+public class HelloWorld2
+{
+	public void coveredByUnitTest() 
+	{
+		System.out.println("coveredByUnitTest1");
+		System.out.println("coveredByUnitTest2");
+	}
+	public void coveredByIntegrationTest() 
+	{
+		System.out.println("coveredByIntegrationTest1");
+		System.out.println("coveredByIntegrationTest2");
+		System.out.println("coveredByIntegrationTest3");
+    }
+}
