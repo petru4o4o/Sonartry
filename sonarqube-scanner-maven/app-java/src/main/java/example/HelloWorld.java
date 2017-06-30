33279e2dfca3d7f3970e025e60d7b145a3fda8d3
petru4o4o <petrol89@gmail.com>
2017-06-30 16:37:50 +0300

    remove fakemethod

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -1,16 +1,7 @@
 package example;
 
 public class HelloWorld {
-	
-  public void fakemethod ()
-  {
-	var target =-5;
-	var num = 3;
 
-	target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-	target =+ num; // Noncompliant; target = 3
-	  
-  }
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
     System.out.println("coveredByUnitTest2");
