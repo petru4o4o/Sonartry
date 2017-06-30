5e984f1cccb054068c1a84bea4b79649accca4b7
petru4o4o <petrol89@gmail.com>
2017-06-30 17:22:24 +0300

    add unexisting methos to UT

--- sonarqube-scanner-maven/app-java/src/test/java/example/HelloWorldUnitTest.java
+++ sonarqube-scanner-maven/app-java/src/test/java/example/HelloWorldUnitTest.java
@@ -7,7 +7,7 @@ public class HelloWorldUnitTest {
   @Test
   public void test() {
     new HelloWorld().coveredByUnitTest();
-	new HelloWorld2().notCovered();
+	new HelloWorld2().1notCovered();
 	new HelloWorld2().coveredByUnitTest();
   }
 
