665d3225f52acfe54b453875f8fcbcd1d756fb7d
petru4o4o <petrol89@gmail.com>
2017-06-30 17:25:39 +0300

    111

--- sonarqube-scanner-maven/app-java/src/test/java/example/HelloWorldUnitTest.java
+++ sonarqube-scanner-maven/app-java/src/test/java/example/HelloWorldUnitTest.java
@@ -7,7 +7,7 @@ public class HelloWorldUnitTest {
   @Test
   public void test() {
     new HelloWorld().coveredByUnitTest();
-	new HelloWorld2().1notCovered();
+	new HelloWorld2().covered();
 	new HelloWorld2().coveredByUnitTest();
   }
 
